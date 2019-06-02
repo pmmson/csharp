ace3d941529d7bcd68806fa1ee220c10c297a8b6
pmmson <pmmson@gmail.com>
2019-06-02 21:10:16 +0300

    Решение 3-х задач ДЗ

---
+++ lesson-2/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Павленко Михаил
+ * 2. Написать метод подсчета количества цифр числа
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task2
+{
+    class Program
+    {
+        static int CountOfNumbersInNumber(long a)
+        {
+            int i = 0;
+            while (a != 0)
+            {
+                a = a / 10;
+                i++;
+            }
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Подсчитаем количество цифр в числе");
+            Console.Write("Введите число: ");
+            long a = Convert.ToInt64(Console.ReadLine());
+            Console.Write($"Количетсво цифр в числе {a}: {CountOfNumbersInNumber(a)}");
+
+            MyLib.Pause();
+        }
+    }
+}
---
+++ lesson-2/Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Павленко Михаил
+ * 
+ * 3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечётных положительных чисел
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Подсчитаем сумму всех введеных нечётных положительных чисел");
+            Console.WriteLine("Для выхода введите \"0\" !");
+            Console.WriteLine($"Сумма всех введеных нечётных положительных чисел: {SumOfOddNumbers()}");
+
+            MyLib.Pause();
+        }
+
+        private static int SumOfOddNumbers()
+        {
+            int i;
+            int sum = 0;
+            do
+            {
+                Console.Write("Введите число: ");
+                i = Convert.ToInt32(Console.ReadLine());
+                if (i > 0 && i % 2 != 0)
+                {
+                    sum += i;
+                }
+            }
+            while (i != 0);
+            return sum;
+        }
+    }
+}
