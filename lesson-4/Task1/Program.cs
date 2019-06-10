5279a4cae4b36f148c126f6b9f3f898911983ad5
pmmson <pmmson@gmail.com>
2019-06-10 21:18:47 +0300

    Создание решения Решение задачи 1 ДЗ урока 4

---
+++ lesson-4/Task1/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Павленко Михаил
+ * 
+ * 1. Дан  целочисленный  массив  из 20 элементов.  Элементы  массива  могут принимать  целые  значения  от –10 000 до 10 000 включительно. 
+ * Заполнить случайными числами.  Написать программу, позволяющую найти и вывести количество пар элементов массива, 
+ * в которых только одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих элемента массива. 
+ * Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+            Random rand = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-10000, 10000);
+            }
+
+            //int[] arr = { 6, 2, 9, -3, 6 }; //for test
+            int count = 0;
+            if (arr.Length % 2 == 0)
+            {
+                for (int i = 0; i < arr.Length; i += 2)
+                {
+                    if (arr[i] % 3 == 0 || arr[i + 1] % 3 == 0)
+                    {
+                        count++;
+                        //Console.Write($"\n({arr[i]}, {arr[i+1]})");
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < arr.Length - 1; i += 2)
+                {
+                    if (arr[i] % 3 == 0 || arr[i + 1] % 3 == 0)
+                    {
+                        count++;
+                        //Console.Write($"\n({arr[i]}, {arr[i + 1]})");
+                    }
+                }
+            }
+
+            PrintArr(arr);
+            Console.WriteLine($"\nКол-во пар элементов массива, в которых только одно число делится на 3: {count}");
+
+            Console.ReadKey();
+        }
+
+        static void PrintArr(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write($"{a[i]} ");
+            }
+        }
+    }
+}
