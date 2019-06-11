f8931b85a8acf402bedf72d841d19be70867e5c5
pmmson <pmmson@gmail.com>
2019-06-11 05:29:29 +0300

    Решение задачи 2 ДЗ урока 4

---
+++ lesson-4/Task2/StaticClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class StaticClass
+    {
+        public static int CountDivByThree(int[] array)
+        {
+            int count = 0;
+            if (array.Length % 2 == 0)
+            {
+                for (int i = 0; i < array.Length; i += 2)
+                {
+                    if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < array.Length - 1; i += 2)
+                {
+                    if (array[i] % 3 == 0 || array[i + 1] % 3 == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
+
