b6a45ab2668a045c80c6c87b63d864d7507aa676
pmmson <pmmson@gmail.com>
2019-06-20 22:59:26 +0300

    старт решения lesson-7 задание 1а,б

---
+++ lesson-7/WF_Udvoitel/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WF_Udvoitel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.clickCounter = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Location = new System.Drawing.Point(607, 168);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(129, 58);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "х2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.BtnCommand2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(607, 274);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(129, 58);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(149, 106);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(24, 25);
+            this.lblNumber.TabIndex = 3;
+            this.lblNumber.Text = "0";
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Location = new System.Drawing.Point(607, 62);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(129, 69);
+            this.btnCommand1.TabIndex = 4;
+            this.btnCommand1.Text = "+1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.BtnCommand1_Click);
+            // 
+            // clickCounter
+            // 
+            this.clickCounter.AutoSize = true;
+            this.clickCounter.Location = new System.Drawing.Point(276, 313);
+            this.clickCounter.Name = "clickCounter";
+            this.clickCounter.Size = new System.Drawing.Size(24, 25);
+            this.clickCounter.TabIndex = 5;
+            this.clickCounter.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 40);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(96, 36);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(324, 38);
+            this.playToolStripMenuItem.Text = "Играть";
+            this.playToolStripMenuItem.Click += new System.EventHandler(this.PlayToolStripMenuItem_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(13, 313);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(257, 25);
+            this.lblInfo.TabIndex = 8;
+            this.lblInfo.Text = "Ваше количество ходов:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.clickCounter);
+            this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Удвоитель";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Label clickCounter;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ lesson-7/WF_Udvoitel/Form1.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Павленко Михаил
+ * 
+ * 1. а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+ *    б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок. 
+ *       Игрок должен получить это число за минимальное количество ходов. 
+ *    в) *Добавить кнопку «Отменить», которая отменяет последние ходы. Используйте обобщенный класс Stack.
+ *    Вся логика игры должна быть реализована в классе с удвоителем.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_Udvoitel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnCommand1_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) + 1).ToString();
+            clickCounter.Text = (int.Parse(clickCounter.Text) + 1).ToString();
+        }
+
+        private void BtnCommand2_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = (int.Parse(lblNumber.Text) * 2).ToString();
+            clickCounter.Text = (int.Parse(clickCounter.Text) + 1).ToString();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            lblNumber.Text = "1";
+            clickCounter.Text = (int.Parse(clickCounter.Text) + 1).ToString();
+        }
+
+        private void PlayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            MessageBox.Show($"Необходимо получить число {r.Next(100)} за минимальное число ходов!");
+        }
+    }
+}
