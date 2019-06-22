e6b4939b19b8446eb0a3b9c4ee133502e1a0bc7a
pmmson <pmmson@gmail.com>
2019-06-22 08:09:59 +0300

    решение задания 2

---
+++ lesson-7/WF_UgadaiN/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WF_UgadaiN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.tBoxEnter = new System.Windows.Forms.TextBox();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblInfoOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(185, 97);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(417, 25);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "Я загадал число от 1 до 100, угадай его!";
+            this.lblInfo.Visible = false;
+            // 
+            // tBoxEnter
+            // 
+            this.tBoxEnter.Location = new System.Drawing.Point(339, 172);
+            this.tBoxEnter.Name = "tBoxEnter";
+            this.tBoxEnter.Size = new System.Drawing.Size(100, 31);
+            this.tBoxEnter.TabIndex = 1;
+            this.tBoxEnter.Visible = false;
+            this.tBoxEnter.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TBoxEnter_MouseMove);
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(307, 231);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(163, 55);
+            this.btnClick.TabIndex = 2;
+            this.btnClick.Text = "угадать!";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Visible = false;
+            this.btnClick.Click += new System.EventHandler(this.BtnClick_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(287, 23);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(211, 51);
+            this.btnNewGame.TabIndex = 3;
+            this.btnNewGame.Text = "Новая игра!";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.BtnNewGame_Click);
+            // 
+            // lblInfoOut
+            // 
+            this.lblInfoOut.AutoSize = true;
+            this.lblInfoOut.Location = new System.Drawing.Point(351, 326);
+            this.lblInfoOut.Name = "lblInfoOut";
+            this.lblInfoOut.Size = new System.Drawing.Size(0, 25);
+            this.lblInfoOut.TabIndex = 4;
+            this.lblInfoOut.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblInfoOut);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.tBoxEnter);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "Form1";
+            this.Text = "Угадай число!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox tBoxEnter;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label lblInfoOut;
+    }
+}
+
---
+++ lesson-7/WF_UgadaiN/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_UgadaiN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        int n;
+
+        private void BtnClick_Click(object sender, EventArgs e)
+        {
+            int userN;
+            bool flag;
+
+            flag = int.TryParse(tBoxEnter.Text, out userN);
+            if (!flag)
+            {
+                lblInfoOut.Text = "Вы не ввели число или введенное число некорректно";
+                lblInfoOut.Left = this.ClientSize.Width / 2 - lblInfoOut.Width / 2;
+            }
+            else
+            {
+                if (userN > n)
+                {
+                    lblInfoOut.Text = "Ваше число больше загаданного";
+                    lblInfoOut.Left = this.ClientSize.Width / 2 - lblInfoOut.Width / 2;
+                }
+                else if (userN < n)
+                {
+                    lblInfoOut.Text = "Ваше число меньше загаданного";
+                    lblInfoOut.Left = this.ClientSize.Width / 2 - lblInfoOut.Width / 2;
+                }
+                else
+                {
+                    lblInfoOut.Text = "Поздравляю! Вы угадали!";
+                    lblInfoOut.Left = this.ClientSize.Width / 2 - lblInfoOut.Width / 2;
+                }
+            }
+
+
+
+            // MessageBox.Show($"{n} {userN}"); // для отладки
+        }
+
+        private void BtnNewGame_Click(object sender, EventArgs e)
+        {
+            lblInfoOut.Text = "";
+            tBoxEnter.Text = "";
+            n = r.Next(1, 100);
+            lblInfo.Visible = true;
+            tBoxEnter.Visible = true;
+            btnClick.Visible = true;
+            lblInfoOut.Visible = true;
+        }
+
+        private void TBoxEnter_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblInfoOut.Text = "";
+        }
+    }
+}
