5810bffcc8e1f900146a11112d60038a4ff2e44e
pmmson <pmmson@gmail.com>
2019-06-24 06:32:16 +0300

    lesson-8.Task1

---
+++ lesson-8/Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Павленко Михаил
+ * 
+ * 1. С помощью рефлексии выведите все свойства структуры DateTime
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(DateTime);
+            Console.WriteLine("Properties DateTime:\n");
+            PropertyInfo[] pInfos = t.GetProperties();
+            foreach (PropertyInfo pInfo in pInfos)
+            {
+                Console.WriteLine($"{pInfo.ToString()};{pInfo.CanRead};{pInfo.CanWrite}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
