267d782f753334c988d6847a3fbe9e1eead2cb0e
pmmson <pmmson@gmail.com>
2019-06-24 06:51:46 +0300

    lesson-8.Task2

---
+++ lesson-8/WF_Task2/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WF_Task2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nUD1 = new System.Windows.Forms.NumericUpDown();
+            this.tBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nUD1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nUD1
+            // 
+            this.nUD1.Location = new System.Drawing.Point(469, 163);
+            this.nUD1.Name = "nUD1";
+            this.nUD1.Size = new System.Drawing.Size(120, 31);
+            this.nUD1.TabIndex = 0;
+            this.nUD1.ValueChanged += new System.EventHandler(this.NUD1_ValueChanged);
+            // 
+            // tBox1
+            // 
+            this.tBox1.Location = new System.Drawing.Point(95, 163);
+            this.tBox1.Multiline = true;
+            this.tBox1.Name = "tBox1";
+            this.tBox1.Size = new System.Drawing.Size(352, 153);
+            this.tBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tBox1);
+            this.Controls.Add(this.nUD1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nUD1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nUD1;
+        private System.Windows.Forms.TextBox tBox1;
+    }
+}
+
---
+++ lesson-8/WF_Task2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NUD1_ValueChanged(object sender, EventArgs e)
+        {
+            tBox1.Text = nUD1.Value.ToString();
+        }
+    }
+}
